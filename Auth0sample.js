4ab470f3bcf0bf151dff387ac504fe8b22ca6781
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-07 14:12:00 -0800

    adding sample for Auth0 additions for Backend

---
+++ Auth0sample.js
@@ -0,0 +1,12 @@
+// Job Model:
+// jobSchema include{
+// email: String
+// };
+
+
+// -------Disregard if not seeding------------
+
+// seed.js:
+// await City.create --> include{
+// email: 'student@email.com'
+// }
